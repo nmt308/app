359d399af3ee52ac8bafa6c2af21356fbeb318e1
nmt308 <minhthanh0946@gmail.com>
2023-03-10 20:53:45 +0700

    first commit

---
+++ lib/pages/signinpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:app/widgets/body.dart';
+
+class SignInPage extends StatelessWidget {
+  static String routeName = "/sign-in";
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Body(),
+    );
+  }
+}
